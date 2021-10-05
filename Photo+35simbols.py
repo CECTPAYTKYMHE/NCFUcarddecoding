d2c838b4cbc46e02790131f3fb32b12a860220af
CECTPAYTKYMHE <bugatti282@gmail.com>
2021-10-05 13:42:13 +0300

    NCFU

---
+++ Photo+35simbols.py
@@ -0,0 +1,45 @@
+import openpyxl
+from openpyxl_image_loader import SheetImageLoader
+
+filein = 'D:\\GIT\\Files\\cpamexport\\student.xlsx'
+#filein = input('Полный путь до файла > ')    
+wb = openpyxl.load_workbook(filein)
+ws = wb['Лист1']
+
+i = 2
+row = ws.max_row
+image_loader = SheetImageLoader(ws)
+try:
+    ws.cell(row=1, column=1).value = 'ID'
+    #ws.cell(row=1, column=6).value = 'Должность'
+    ws.cell(row=1, column=8).value = 'Фотография №'
+    while i != row: #перебор значений 4 столбца, перекодировка и запись обратно в книгу
+        ID = ws.cell(row=i, column=1)
+        ID = str(ID.value)
+        ID = ID.replace('-','')
+        image = image_loader.get('F' + str(i)) #клетка с фотографией
+        image.save('d:\\git\\files\\photo\\' + ID + '.jpg') #экспорт фото
+        ws.cell(row=i, column=8).value = ID +'.jpg' #имяфото.jpg
+        ws.cell(row=i, column=1).value = ID #удаление '-' в NCFUGUID
+        ws._images = []
+        cell_obj = ws.cell(row=i, column=5)
+        if len(cell_obj.value) > 35:
+            lenght = len(cell_obj.value)
+            cell_obj = cell_obj.value.split()
+            n = len(cell_obj) / 2
+            n = int(n)
+            part1 = cell_obj[0:n + 1]
+            part2 = cell_obj[n + 1:len(cell_obj)]
+            part1 = ' '.join(part1)
+            part2 = ' '.join(part2)
+            if len(part1) > 35 or len(part2) > 35:
+                print(f'"{part1}" или "{part2}" больше 35 символов')
+                break
+            else:
+                ws.cell(row=i, column=5).value = part1
+                ws.cell(row=i, column=6).value = part2
+        i += 1
+    
+    wb.save(filename = filein)
+except:
+    print('Что-то пошло не так!')
--- photoexport.py
+++ photoexport.py
@@ -3,7 +3,7 @@ from openpyxl import load_workbook
 from openpyxl_image_loader import SheetImageLoader
 
 #filein = input('Полный путь до файла > ') 
-filein = 'D:\\GIT\\Files\\cpamexport\\employ.xlsx'
+filein = 'D:\\GIT\\Files\\cpamexport\\student.xlsx'
 wb = openpyxl.load_workbook(filein)
 ws = wb['Лист1']
 
@@ -11,11 +11,11 @@ ws = wb['Лист1']
 i = 3 #начальная строка
 row = ws.max_row
 image_loader = SheetImageLoader(ws)
-while i != row: #проверить конечную строку!!!!
+while i != row + 1: #проверить конечную строку!!!!
     ID = ws.cell(row=i, column=1)
     ID = str(ID.value)
     ID = ID.replace('-','')
-    image = image_loader.get('F' + str(i)) #клетка с фотографией
+    image = image_loader.get('E' + str(i)) #клетка с фотографией
     image.save('d:\\git\\files\\photo\\' + ID + '.jpg') #экспорт фото
     ws.cell(row=i, column=5).value = ID +'.jpg' #имяфото.jpg
     ws.cell(row=i, column=1).value = ID #удаление '-' в NCFUGUID
