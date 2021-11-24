aecb6e2d4597cc0d141218b92406ad2d651b3b10
CECTPAYTKYMHE <bugatti282@gmail.com>
2021-11-24 16:34:40 +0300

    NCFU

--- photoexport mass student.py
+++ photoexport mass student.py
@@ -1,9 +1,14 @@
 import openpyxl
 from openpyxl_image_loader import SheetImageLoader
 import os
+import csv
+from PIL import Image
 cwd = os.path.abspath('D:\\GIT\\Files\\student\\2 курс очная форма')
 files = os.listdir(cwd)
-
+Image.MAX_IMAGE_PIXELS = None
+logs = open(f'D:\\GIT\\Files\\student\\errorlogs{cwd[21:22]}.csv', 'w')
+logs.write('Ошибка фотографии в файле, Строка, ФИО\n')
+logs.close()
 for file in files:
     try:
         filein = cwd + '\\' + file
@@ -18,21 +23,23 @@ for file in files:
         ws.cell(row=1, column=4).value = 'Отчество'
         ws.cell(row=1, column=5).value = 'Фотография №'
         ws.cell(row=1, column=6).value = 'Role'
-       #ws.cell(row=1, column=7).value = ''
         while i != row + 1: #проверить конечную строку!!!!
             ID = ws.cell(row=i, column=1)
             ID = str(ID.value)
             ID = ID.replace('-','')
             ID = ID.upper()
-            ws.unmerge_cells(start_row=i, start_column=5, end_row=i, end_column=6)
+            try:
+                ws.unmerge_cells(start_row=i, start_column=5, end_row=i, end_column=6)
+            except:
+                pass
             try:
                 image = image_loader.get('E' + str(i)) #клетка с фотографией
                 image.save('d:\\git\\files\\student\\photo\\' + ID + '.jpg') #экспорт фото
                 ws.cell(row=i, column=5).value = ID + '.jpg' #имяфото.jpg
             except:
-                logs = open('D:\\GIT\\Files\\student\\errorlogs.txt', 'a')
-                print(f'<Неправильная фотография в файле {cwd[21:]}\{file} в строке {str(i)} у студента {ws.cell(row=i, column=2).value} {ws.cell(row=i, column=3).value} {ws.cell(row=i, column=4).value}>')
-                logs.write(f'<Неправильная фотография в файле {cwd[21:]}{file} в строке {str(i)} у студента {ws.cell(row=i, column=2).value} {ws.cell(row=i, column=3).value} {ws.cell(row=i, column=4).value}>\n')
+                logs = open(f'D:\\GIT\\Files\\student\\errorlogs{cwd[21:22]}.csv','a')
+                print(f'<Неправильная фотография в файле {cwd[21:]}\{file} в строке {i-1} у студента {ws.cell(row=i, column=2).value} {ws.cell(row=i, column=3).value} {ws.cell(row=i, column=4).value}>')
+                logs.write(f'{cwd[21:]}\\{file}, {i-1}, {ws.cell(row=i, column=2).value} {ws.cell(row=i, column=3).value} {ws.cell(row=i, column=4).value} \n')
                 logs.close()
                 pass
             ws.cell(row=i-1, column=6).value = 'Student'
