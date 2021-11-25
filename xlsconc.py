66c87eaca8bdc7d13d70a967d5c28c20dbb6a327
CECTPAYTKYMHE <bugatti282@gmail.com>
2021-11-25 14:20:45 +0300

    NCFU

--- xlsconc.py
+++ xlsconc.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-cwd = os.path.abspath('D:\\GIT\\card')
+cwd = os.path.abspath('D:\\GIT\\Files\\student\\2 курс очная форма')
 files = os.listdir(cwd)
 filesdata = []
 for file in files:
@@ -12,4 +12,4 @@ for file in files:
 
 appended_xlsx = pd.concat(filesdata)
 
-appended_xlsx.to_excel('D:\\GIT\\card\\allemployee.xlsx', index=False)
+appended_xlsx.to_excel('D:\\GIT\\Files\\student\\2 курс очная форма\\course2.xlsx', index=False)
