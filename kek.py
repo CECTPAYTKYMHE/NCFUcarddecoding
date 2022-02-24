5063e27964d5eeab801307ffc733c5640935ec72
CECTPAYTKYMHE <bugatti282@gmail.com>
2022-02-24 09:35:20 +0300

    last

---
+++ kek.py
@@ -0,0 +1,7 @@
+﻿url = 'https://www.olx.ua/d/obyavlenie/bts-ukrndiagroproekt-arenda-ofisa-73-m-kv-na-solomenskoy-ploschadi-2-IDLRtAF.html#d87ad13fbe;promoted'
+url =url.split('#')[0]
+print(url)
+import json
+phone = b'{"data":{"phones":["063 729 7450"]}}'
+phone = json.loads(phone)
+print(phone['data']['phones'][0])
---
+++ pars.py
@@ -0,0 +1,23 @@
+﻿from gettext import find
+from xml.dom.minidom import Element
+from selenium import webdriver as wd
+import time
+
+url = 'https://instagram.com'
+options = wd.ChromeOptions()
+options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36')
+driver = wd.Chrome(executable_path='C:/GIT/files/chromiumdriver/chromedriver.exe',options=options)
+
+try:
+    driver.get(url=url)
+    time.sleep(5)
+ 
+    login_input = driver.find_element_by_name('username')
+    login_input.click()
+    login_input.send_keys('89187591088')
+    time.sleep(30)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
